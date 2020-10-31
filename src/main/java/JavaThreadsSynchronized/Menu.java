b94428e36ec5afa33d03835ef58640d263f8c1d0
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-10-31 09:17:57 -0600

    Agregar comentarios

--- src/main/java/JavaThreadsSynchronized/Menu.java
+++ src/main/java/JavaThreadsSynchronized/Menu.java
@@ -1,4 +1,18 @@
-
+/*
+Sistema de un restaurante.
+El número de cocineros son 6
+10 camareros que también serán hilos cada uno. 
+1 cajero que se encarga de ingresar los pedidos que son enviados inmediatamente a la cocina y
+estos deben ser tomados por los chefs y preparar la comida. Esta mesa solo tiene espacio para 5 platos.
+Si hay más platos el chef tiene que esperar para ponerlo en la mesa. 
+Cada uno de los meseros toma el plato y dura entre 2000 y 6000 milisegundos en entregar cada orden. 
+El menú del restaurante es 
+Hamburguesa que tarda 3000 milisegundos en ser preparada
+Chalupas tarda 5000 milisegundos
+Papas fritas tarda 2000 milisegundos
+Perros calientes 4000 milisegundos
+Cazados 6000 milisegundos. 
+*/
 package JavaThreadsSynchronized;
 
 
